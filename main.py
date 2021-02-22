7b5d5d07fbfaf7be5e715bee5f78a09aa908c5e1
yOn3l <74076866+yOn3l@users.noreply.github.com>
2021-02-22 17:43:24 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+import datetime
+
+bot = commands.Bot(command_prefix='*')
+
+@bot.command()
+async def ping(ctx):
+   await ctx.send('pong')
+
+@bot.command()
+async def suma(ctx, num1: int, num2: int):
+   await ctx.send(num1 + num2)
+
+@bot.command()
+async def huevo(ctx):
+   await ctx.send('comes')
+
+@bot.command()
+async def resta(ctx, num1: int, num2: int):
+   await ctx.send(num1 - num2)
+
+@bot.command()
+async def info(ctx):
+   embed = discord.Embed(title =f"{ctx.guild.name}", descripcion="culo srever a xd", timestamp=datetime.datetime.utcnow())
+   await ctx.send(embed=embed)
+
+
+@bot.command()
+async def av(ctx, member: discord.Member):
+   avatar = discord.Embed(
+      color = discord.Color.green()
+   )
+   avatar.set_image(url="{}".format(member.avatar_url))
+
+   await ctx.send(embed=avatar)
+
+
+@bot.event
+async def on_ready():
+    print('el bot esta prendido')
+    game = discord.Game("comiendo")
+    await bot.change_presence(activity=game)
+
+
+
+bot.run('insert token')
